70c304d5522b795ddee0076b94bd3710491fa4d7
Jonathan <jonsasser@gmail.com>
2024-07-05 11:22:06 -0700

    SpringProjectCreated

---
+++ SportingEventsPAGroup/src/main/java/com/jsass/sportingeventspagroup/SportingEventsPaGroupApplication.java
@@ -0,0 +1,13 @@
+package com.jsass.sportingeventspagroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportingEventsPaGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportingEventsPaGroupApplication.class, args);
+	}
+
+}
---
+++ SportingEventsPAGroup/src/test/java/com/jsass/sportingeventspagroup/SportingEventsPaGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsass.sportingeventspagroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportingEventsPaGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
